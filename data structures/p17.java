5954cf0e21541d4566dac06c8b6368375e487143
Shibil-Basith <84259186+Shibil-Basith@users.noreply.github.com>
2024-06-22 10:43:57 +0530

    Update p17.java

--- data structures/p17.java
+++ data structures/p17.java
@@ -1,57 +1,71 @@
 import java.util.Scanner;
 
-public class p17{
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the number of elements: ");
-        int n = scanner.nextInt();
-
-        int[] arr = new int[n];
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+class p17 {
+    public static int[] quickSort(int[] array, int l, int h) {
+        if (l < h) {
+            int j = partition(array, l, h);
+            quickSort(array, l, j - 1);
+            quickSort(array, j + 1, h);
         }
-
-        quickSort(arr, 0, arr.length - 1);
-
-        System.out.println("Sorted array:");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-
-        scanner.close();
+        return array;
     }
 
-    public static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
+    public static void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
 
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+    public static int partition(int[] array, int l, int h)
+{
+    int pivot = array[l];
+    int i = l+1;
+    int j = h;
+    while (i <= j) {
+        while (i <= j && array[i] <= pivot) {
+            i++;
+        }
+        while (j >= i && array[j] > pivot) {
+            j--;
+        }
+        if (i < j) {
+            swap(array, i, j);
         }
     }
 
-    private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
-
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-
-        return i + 1;
-    }
+    swap(array, l, j);
+    return j;
 }
 
+
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int size = input.nextInt();
+        int array[] = new int[size];
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i < size; i++) {
+            array[i] = input.nextInt();
+        }
+
+        // Print array before sorting
+        System.out.println("Array before sorting: ");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Perform quicksort
+        quickSort(array, 0, array.length - 1);
+
+        // Print array after sorting
+        System.out.println("Array after sorting: ");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Close the scanner
+        input.close();
+    }
+}
